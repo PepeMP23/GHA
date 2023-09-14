14d448cb6a077d8ad9f47d9af1c2752452d4c729
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-09-14 14:23:43 -0600

    MRC header and footer

---
+++ MRC/js/loader-header-footer.js
@@ -0,0 +1,19 @@
+// Function to load the header and footer
+function loadHeaderAndFooter() {
+    // Load the header content
+    fetch('header.html')
+        .then(response => response.text())
+        .then(headerHtml => {
+            document.getElementById('header-placeholder').innerHTML = headerHtml;
+        });
+    
+    // Load the footer content
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(footerHtml => {
+            document.getElementById('footer-placeholder').innerHTML = footerHtml;
+        });
+}
+
+// Call the function to load the header and footer
+loadHeaderAndFooter();
