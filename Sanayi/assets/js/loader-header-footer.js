8ee2546b6976b0bdfff62b7d3759471e533025ee
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-11-21 08:18:57 -0600

    Correcciones

--- Sanayi/assets/js/loader-header-footer.js
+++ Sanayi/assets/js/loader-header-footer.js
@@ -2,11 +2,11 @@
 function loadHeaderAndFooter() {
     // Load the header content
     
-    fetch('header.html')
+    /*fetch('header.html')
         .then(response => response.text())
         .then(headerHtml => {
             document.getElementById('header-placeholder').innerHTML = headerHtml;
-        });
+        });*/
     
     // Load the footer content
     fetch('footer.html')
