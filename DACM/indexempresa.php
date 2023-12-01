e51b9272d1fdf0f2a4f0b0db5ad25316abaf84b1
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-12-01 16:45:54 -0600

    Correcciones

---
+++ DACM/indexempresa.php
@@ -0,0 +1,90 @@
+<div class="container-fluid animated fadeIn wow py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h1 class="mb-4">DACM <br>
+                    DESARROLLADORES EN AUTOMATIZACIÓN Y CONTROL DE MOVIMIENTO</h1>
+                <p> 
+                    Somos una empresa mexicana especializada en diseño, automatización 
+                    y control para la manufactura. 
+                    Nos especializamos en brindar soluciones para la industria 4.0.
+                    Con un enfoque centrado en el cliente nos distinguimos por brindar 
+                    soluciones personalizadas que optimizan los procesos de producción 
+                    y mejoran la eficiencia operativa.
+                </p>
+                <audio class="d-block mx-auto mb-5 mb-md-0" controls>
+                    <source src="https://dacm.mx/audio/audio-somos-dacm-automatizacion.mp3" type="audio/mpeg">
+                </audio>
+            </div>
+            <div class="col-12 col-md-6 col-sm-10">
+                <iframe style="height:315px;width:100%;border: 0px;" src="https://www.youtube.com/embed/B5_F6QTw33M" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid animated fadeIn wow py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4">
+                <div class="mb-4 text-center">
+                    <lord-icon
+                        src="https://cdn.lordicon.com/tbfrtmlu.json"
+                        trigger="loop"
+                        delay="600"
+                        colors="primary:#000000,secondary:#c42626"
+                        class="iconoanimado">
+                    </lord-icon>
+                </div>
+                <h2 class="text-center">MISIÓN</h2>
+                <p class=" mb-5 mb-md-0 animated fadeIn wow">
+                    Ser líderes en el desarrollo de soluciones 
+                    de automatización y control para la industria manufacturera en México, brindando a 
+                    nuestros clientes soluciones personalizadas que mejoren su eficiencia operativa y los 
+                    impulsen hacia la era de la Industria 4.0.
+                </p>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="mb-4 text-center">
+                    <lord-icon
+                        src="https://cdn.lordicon.com/sbiheqdr.json"
+                        trigger="loop"
+                        delay="600"
+                        colors="primary:#c42626,secondary:#000000"
+                        class="iconoanimado">
+                    </lord-icon>
+                </div>
+                <h2 class="text-center">VISIÓN</h2>
+                <p class="mb-5 mb-md-0 animated fadeIn wow delay-1s">
+                    Ser reconocidos como la empresa líder en diseño, automatización y 
+                    control de movimiento en México, proporcionando soluciones innovadoras y de vanguardia 
+                    que permitan a nuestros clientes optimizar sus procesos de producción y mantenerse a 
+                    la vanguardia de la industria.
+                </p>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="mb-4 text-center">
+                    <lord-icon
+                        src="https://cdn.lordicon.com/ytuosppc.json"
+                        trigger="loop"
+                        delay="600"
+                        colors="primary:#000000,secondary:#c42626"
+                        class="iconoanimado">
+                    </lord-icon>
+                </div>
+                <h2 class="text-center">NUESTROS VALORES</h2>
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <ul class="mb-0 animated fadeIn wow delay-2s">
+                            <li>Calidad</li>
+                            <li>Servicio al cliente</li>
+                            <li>Innovación</li>
+                            <li>Profesionalismo</li>
+                            <li>Trabajo en equipo</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ DACM/indexwa.php
@@ -0,0 +1,5 @@
+<div id="botonWhatsApp">
+    <a href="https://wa.me/524791035264" target="_blank">
+        <img src="/imagenes/icono-whatsapp.svg" alt="Whatsapp" class="w-100">
+    </a>
+</div>
